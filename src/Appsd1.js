4001b531abc57ff97d1d9cb59915106fb840697a
Parminder <parminder.singh.coder@gmail.com>
2024-09-17 20:46:08 +1200

    Component testing

---
+++ src/Appsd1.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Component } from 'react';
+
+class Appsd1 extends Component() {
+ render(){
+  return (
+    <div><h2>APP2 Class Component</h2></div>    
+      
+  );
+ }
+}
+
+export default Appsd1;
---
+++ src/com/App1.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Component } from "react";
+
+class App1 extends Component{
+    render(){
+        return(
+            <div>App1.js Component</div>
+        );
+    }
+}
+export default App1;
